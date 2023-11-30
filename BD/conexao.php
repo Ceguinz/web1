d82cd0410dedcab5a074b13ce1e503d03f103c75
Vinícius do Carmo Silva <viniciusvgp96@gmail.com>
2023-11-29 23:36:57 -0300

    first commit

---
+++ BD/conexao.php
@@ -0,0 +1,5 @@
+<?php
+$utf8 = header("Content-Type: text/html; charset=utf-8");
+$link = new mysqli('localhost','id21487249_root','Ifmg1234@','id21487249_db_clientes');
+$link->set_charset('utf8');
+?>
